444fda1bd1ed4a85a281e7e8a4c65b615aeb29dc
Javantax <79591148+Javantax@users.noreply.github.com>
2025-05-24 20:07:13 -0300

    Add broilerplate code

---
+++ src/main/java/com/example/minilinkify/MinilinkifyUrlApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.minilinkify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinilinkifyUrlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinilinkifyUrlApiApplication.class, args);
+	}
+
+}
