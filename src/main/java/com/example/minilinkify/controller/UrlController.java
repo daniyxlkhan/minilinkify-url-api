83b77bd4a83ed301f8ca7ced897315a140cc85da
Javantax <79591148+Javantax@users.noreply.github.com>
2025-05-30 20:59:00 -0300

    Connect frontend with backend

--- src/main/java/com/example/minilinkify/controller/UrlController.java
+++ src/main/java/com/example/minilinkify/controller/UrlController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:63342")
 @RestController
 @RequestMapping(path = "shorten")
 public class UrlController {
