603f348e4a51ff2e567c85bd3997daa6cc0f3446
Javantax <79591148+Javantax@users.noreply.github.com>
2025-05-30 21:03:14 -0300

    Update pom.xml

---
+++ src/test/java/com/example/minilinkify/MinilinkifyUrlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minilinkify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinilinkifyUrlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
